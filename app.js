124513d308e383cb51d799760fc42d32a4889ae7
MasumAhmed19 <masum.ahmed1328@gmail.com>
2024-09-13 23:59:28 +0600

    more gsap

--- app.js
+++ app.js
@@ -86,3 +86,27 @@ function firstAnim(){
 }
 
 firstAnim()
+
+
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    elem.addEventListener("mousemove", function(dets){
+        var diff = (dets.clientY - elem.getBoundingClientRect().top)
+        gsap.to(elem.querySelector("img"), {
+            opacity:1,
+            ease: Power1,
+            top: diff,
+            left: dets.clientX,
+        })
+    })
+})
+
+
+document.querySelectorAll(".elem").forEach(function(elem){
+    elem.addEventListener("mouseleave", function(dets){
+        gsap.to(elem.querySelector("img"), {
+            opacity:0,
+            ease: Power1,
+        })
+    })
+})
